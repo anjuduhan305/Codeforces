6ef0bed031d5887fdcbab98135b70bfe20ca9d89
Anju Duhan <anjuduhan305@gmail.com>
2025-12-25 11:34:36 +0530

    Create ThreeIndices.py

---
+++ ThreeIndices.py
@@ -0,0 +1,52 @@
+'''
+Problem Name : Three Indices
+
+Problem No. : 1380/A
+
+Description : You are given a permutation p1,p2,…,pn. Recall that sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
+Find three indices i, j and k such that: 1≤i<j<k≤n; pi<pj and pj>pk. Or say that there are no such indices.
+Input The first line contains a single integer T(1≤T≤200) — the number of test cases.
+Next 2T lines contain test cases — two lines per test case. The first line of each test case contains the single integer n(3≤n≤1000) — the length of the permutation p.
+The second line contains n integers p1,p2,…,pn(1≤pi≤n; pi≠pjif i≠j) — the permutation p.
+Output For each test case: if there are such indices i, j and k, print YES (case insensitive) and the indices themselves;if there are no such indices, print NO (case insensitive).
+If there are multiple valid triples of indices, print any of them.
+
+Example:
+Input
+3
+4
+2 1 4 3
+6
+4 6 1 2 5 3
+5
+5 3 1 2 4
+Output
+YES
+2 3 4
+YES
+3 5 6
+NO
+
+My Approach :Run the loop for (length of the permutation array − 2) times. In the first iteration, select the value at the first index and check it with the next two values.
+If the condition becomes true, return the indices, print "Yes", and break the loop.Otherwise, move to the next index value and again check it with the next two values.
+Repeat this process until the condition is satisfied. If no such indices are found, print "No".
+
+Time complexity : O(t × n)
+Space complexity : O(n)
+
+'''
+
+test_cases = int(input())
+for _ in range(test_cases):
+    n_element = int(input())
+    p = map(int, input().split())
+    p = list(p)
+    i = 1
+    while i < (n_element-1):
+        if p[i-1] < p[i] and p[i] > p[i+1]:
+            print('Yes')
+            print(i, i+1, i+2)
+            break
+        i += 1
+    else:
+        print('No')
